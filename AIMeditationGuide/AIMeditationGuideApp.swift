4c38b1326a2520f94732a0293c6910b1c61c06c3
vecher <vecherkovsky2003@mail.ru>
2025-12-29 14:56:14 +0300

    Initial Commit

---
+++ AIMeditationGuide/AIMeditationGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMeditationGuideApp.swift
+//  AIMeditationGuide
+//
+//  Created by  Vladislav on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIMeditationGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
